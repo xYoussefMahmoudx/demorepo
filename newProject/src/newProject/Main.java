87cb6a93ce8fd2825704b4f324821e6d461265f3
anass-zikry <anasszikry@gmail.com>
2022-05-09 18:25:14 +0200

    fifff

--- newProject/src/newProject/Main.java
+++ newProject/src/newProject/Main.java
@@ -1,5 +1,5 @@
 package newProject;
 
 public class Main {
-
+	
 }
---
+++ newProject/src/newProject/Mk.java
@@ -0,0 +1,9 @@
+package newProject;
+
+public class Mk {
+
+	public static void main(String[] args) {
+		System.out.println("Heeloo");
+	}
+
+}
